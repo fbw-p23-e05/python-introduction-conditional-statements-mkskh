1eebecb43507c3afff7d9136babf58cfbc8f96bb
mkskh <maksym.kharchenko@dci-student.org>
2023-08-24 23:11:37 +0200

    completed task5

---
+++ task5.py
@@ -0,0 +1,6 @@
+target_num = 7
+guess_num = int(input('Guess a number between 1 and 10 until you get it right :'))
+
+while target_num != guess_num:
+    guess_num = int(input('Guess a number between 1 and 10 until you get it right :'))
+print('Well guessed')
