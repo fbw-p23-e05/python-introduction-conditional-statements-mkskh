c52ed13355dcfb2baa1d65cf7714b04aa6a03c0e
mkskh <maksym.kharchenko@dci-student.org>
2023-08-24 18:13:15 +0200

    compleed task3

---
+++ task3.py
@@ -0,0 +1,8 @@
+a = int(input('Number to check:'))
+
+if a%2==1:
+    print(a, 'is an odd number!')
+elif a%2==0:
+    print(a, 'is an even number!')
+else:
+    None # Do I always have to write Else at the end if I don't need to? (in some cases)
