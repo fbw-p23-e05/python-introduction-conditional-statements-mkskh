f6efbd9b3270b39446903b4885b313b05820d3b0
mkskh <maksym.kharchenko@dci-student.org>
2023-08-24 18:02:41 +0200

    compleed task2

---
+++ task2.py
@@ -0,0 +1,7 @@
+a = int(input('First number: '))
+b = int(input('Second number: '))
+
+if a > b:
+    print('The result of calculation is:', ((a - b) * 2))
+else:
+    print('The result of calculation is:', abs(a - b))
